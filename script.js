8f71fafaad87dfb79773666fd8cbe301f0186ac9
JonasLeiserson <49516747@est.ort.edu.ar>
2024-08-01 11:26:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("pollo")
+var atkPICKCHEL = 100
+var vidaPICKCHEL = 200
+var velocidadPICKCHEL = 300
+var Tipo1PICKCHEL = "ELECTRICO" 
+
+var atkOWLLEON = 200
+var vidaOWLLEON = 150
+var velocidadOWLLEON = 200
+var Tipo1OWLLEON = "FUEGO"
+
+var atkROKTOM = 200
+var vidaROKTOM = 400
+var velocidadROKTOM = 100
+var Tipo1ROKTOM = "ROCA"
+
+var atkMACETONIO = 200
+var vidaMACETONIO = 400
+var velocidadMACETONIO = 100
+var Tipo1MACETONIO= "PLANTA"
