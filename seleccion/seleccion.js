bf7324e680b6eee1d3befb29831af1ab4b4f986b
JonasLeiserson <49516747@est.ort.edu.ar>
2024-08-07 11:34:37 -0300

    AREGLANDO LAS ESTADIDSTICAS

--- seleccion/seleccion.js
+++ seleccion/seleccion.js
@@ -1,9 +1,9 @@
-var PICKCHEL = 
-{
+var PICKCHEL = {
     nombre: "PICKCHEL",
     atk: 100,
     vida: 200,
     velocidad: 300,
+    defensa: 200,
     Tipo1: "ELECTRICO",
     Habilidad1: 1,
     Habilidad2: 1,
@@ -15,6 +15,7 @@ var OWLEON = {
     atk: 200,
     vida: 150,
     velocidad: 200,
+    defensa: 200,
     Tipo1: "FUEGO",
     Habilidad1: 1,
     Habilidad2: 1,
@@ -26,6 +27,7 @@ var ROKTOM = {
     atk: 200,
     vida: 400,
     velocidad: 100,
+    defensa: 200,
     Tipo1: "ROCA",
     Habilidad1: 1,
     Habilidad2: 1,
@@ -37,36 +39,52 @@ var MACETONIO = {
     atk: 200,
     vida: 400,
     velocidad: 100,
+    defensa: 200,
     Tipo1: "PLANTA",
     Habilidad1: 1,
     Habilidad2: 1,
     Habilidad3: 1
 };
+
+var MELONKEY = {
+    nombre: "MELONKEY",
+    atk: 150,
+    vida: 180,
+    velocidad: 250,
+    defensa: 200,
+    Tipo1: "AGUA",
+    Habilidad1: 1,
+    Habilidad2: 1,
+    Habilidad3: 1
+};
+
 var PokeORTS = {
     "Owleon": OWLEON,
-    "Melonkey": Melonkey,
+    "Melonkey": MELONKEY,
     "Roktom": ROKTOM,
     "Pickchel": PICKCHEL,
     "Macetonio": MACETONIO
 };
 
-
-//FALTA MELONKEY PIPA
-
-function cambiarpokeort(button) 
-{
-button.style.display = "none" ; 
-const imgSrc = button.querySelector('img').src;
-const selectedPokeortDisplay = document.getElementById('selected-pokeort-display');
-selectedPokeortDisplay.src = imgSrc;
+function cambiarpokeort(button) {
+    button.style.display = "none"; 
+    const imgSrc = button.querySelector('img').src;
+    const selectedPokeortDisplay = document.getElementById('selected-pokeort-display');
+    selectedPokeortDisplay.src = imgSrc;
 }
 
 function MostrarEstadisticas(button) {
-    const pokeortId = button.querySelector('.pokeort-name').textContent;
+    const pokeortId = button.querySelector('.pokeort-name').textContent.trim();
     const pokeort = PokeORTS[pokeortId];
 
-    document.getElementById("VIDA").textContent = "VIDA: " + pokeort.vida;
-    document.getElementById("VELOCIDAD").textContent = "VELOCIDAD: " + pokeort.velocidad;
-    document.getElementById("DANO").textContent = "DAÑO: " + pokeort.atk;
-    document.getElementById("DEFENSA").textContent = "DEFENSA: " + (pokeort.defensa); 
+    if (pokeort) {
+        document.getElementById("VIDA").textContent = "VIDA: " + pokeort.vida;
+        document.getElementById("VELOCIDAD").textContent = "VELOCIDAD: " + pokeort.velocidad;
+        document.getElementById("DAÑO").textContent = "DAÑO: " + pokeort.atk;
+        document.getElementById("DEFENSA").textContent = "DEFENSA: " + pokeort.defensa;
+
+       
+    } else {
+        console.error("PokeORT no encontrado:", pokeortId);
+    }
 }
