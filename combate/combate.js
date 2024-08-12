02de27f6802c5159118fbcf83f6ac25aaf8b98c3
yuigul <sabiojuancruz2@gmail.com>
2024-08-12 11:23:24 -0300

    LO LOGRE SEÑOR, LO LOGRE

---
+++ combate/combate.js
@@ -0,0 +1,9 @@
+function mostrar_ataques() {
+    document.getElementById("ataques").style.display = "flex";
+    document.getElementById("cambiar-pokeort").style.display = "none";
+}
+
+function mostrar_pokeort() {
+    document.getElementById("ataques").style.display = "none";
+    document.getElementById("cambiar-pokeort").style.display = "flex";
+}
