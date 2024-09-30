5c3cb42f10d06b615831ca29ecdfc315be676999
JonasLeiserson <49516747@est.ort.edu.ar>
2024-09-30 09:37:50 -0300

    arreglo

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-
 app.use(cors({
     origin: '*', 
     credentials: false 
@@ -12,18 +11,18 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-app.get('/leer-datos', (res) => {
-    fs.readFile('DatosPokeorts.json', 'utf8', (data) => {
+app.get('/leer-datos', (req, res) => {
+    fs.readFile('DatosPokeorts.json', 'utf8', (err, data) => {
         res.send(data);
     });
 });
 
 app.post('/guardar-datos', (req, res) => {
-    fs.readFile('DatosPokeorts.json', 'utf8', (data) => {
+    fs.readFile('DatosPokeorts.json', 'utf8', (err, data) => {
         let datosExistentes = JSON.parse(data);
         const nuevosDatos = req.body;
 
-        
+        // Actualizar datos
         datosExistentes.Pokeort1 = nuevosDatos.Pokeort1 || datosExistentes.Pokeort1;
         datosExistentes.Pokeort2 = nuevosDatos.Pokeort2 || datosExistentes.Pokeort2;
         datosExistentes.Pokeort3 = nuevosDatos.Pokeort3 || datosExistentes.Pokeort3;
@@ -31,8 +30,8 @@ app.post('/guardar-datos', (req, res) => {
         datosExistentes.PokeortEnemigo2 = nuevosDatos.PokeortEnemigo2 || datosExistentes.PokeortEnemigo2;
         datosExistentes.PokeortEnemigo3 = nuevosDatos.PokeortEnemigo3 || datosExistentes.PokeortEnemigo3;
 
-    
         fs.writeFile('DatosPokeorts.json', JSON.stringify(datosExistentes, null, 2), (err) => {
+            
             res.send('Datos guardados con éxito.');
         });
     });
